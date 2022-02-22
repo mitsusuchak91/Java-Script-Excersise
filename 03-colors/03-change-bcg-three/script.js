db8d06e6b17384646c417c338d61c955c62a144b
aliciams <# apt-get install git>
2022-02-22 23:38:28 +0100

    added files

---
+++ 03-colors/03-change-bcg-three/script.js
@@ -0,0 +1,19 @@
+/* becode/javascript
+ *
+ * /03-colors/03-change-bcg-three/script.js - 3.3: couleur de fond (3)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(() => {
+    // your code here
+    document.getElementById("run").addEventListener("click",() => {
+        let randomColor = Math.floor(Math.random()*16777215).toString(16);
+        //generates random color
+        document.body.style.background = "#" + randomColor;
+    });
+})();
