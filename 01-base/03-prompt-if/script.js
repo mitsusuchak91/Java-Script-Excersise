f337181f3be0ae57df07a6d980425dc7c748f8ae
aliciams <# apt-get install git>
2022-02-22 23:33:12 +0100

    added file

---
+++ 01-base/03-prompt-if/script.js
@@ -0,0 +1,22 @@
+/* becode/javascript
+ *
+ * /01-base/03-prompt-if/script.js - 1.3: promp & if
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(() => {
+    // your code here
+    var cake = prompt("Do you want this cake?");
+
+            if(cake == 'yes' ) {
+            alert("Congratulations!");
+            }
+            else { 
+            alert("You can not refuse this cake!");
+            }
+})();
